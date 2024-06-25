152f23b0474af99f6b6ac3854db460ab68ad304e
Nath Duncan <nathan.duncan@outserveweb.co.uk>
2024-06-25 15:13:40 +0100

    index pages

---
+++ src/components/sections/index.ts
@@ -0,0 +1,6 @@
+import { StorySoFar } from "./story";
+import { Contact } from "./contact";
+import { LandingPage } from "./landingPage";
+import { OpeningTimes } from "./openingTImes";
+
+export { StorySoFar, Contact, LandingPage, OpeningTimes };
