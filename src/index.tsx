025cf0a2a557809eca544cac4f56fc5dc504cf08
Nath Duncan <nathan.duncan@outserveweb.co.uk>
2024-06-25 15:13:57 +0100

    add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import ErrorPage from "./errorPage";
+import { OpeningTimes, StorySoFar, Contact } from "./components/sections";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/openinghours",
+    element: <OpeningTimes />,
+  },
+  {
+    path: "/our-story",
+    element: <StorySoFar />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("ms_page") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
