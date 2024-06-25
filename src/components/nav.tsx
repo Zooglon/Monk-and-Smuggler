05c225c2ea83e6ccbecf514db6bac2036da04615
Nath Duncan <nathan.duncan@outserveweb.co.uk>
2024-06-25 10:33:34 +0100

    build out Nav component

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,1 +1,60 @@
-export const Nav = () => <p>I am a header</p>;
+import { useState } from "react";
+
+export const Nav = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <nav id="ms_mobile_nav" className="ms_nav flex col">
+      {showNav ? (
+        <>
+          <div className="flex row between p-3">
+            <aside className="ms_nav--aside">
+              <p style={{ animationDelay: "0.5s" }}>Hello Stafford.</p>
+              <p style={{ animationDelay: "1.5s" }}>You are loved.</p>
+              <p style={{ animationDelay: "2.5s" }}>You are welcome here.</p>
+            </aside>
+            <button onClick={() => setShowNav(false)} className="ms_nav--Btn ms_Btn flex col gap-s">
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+          </div>
+          <ul className="ms_nav--container">
+            <li className="ms_nav--menu-item">
+              <a href="#os_main" onClick={() => setShowNav(false)}>
+                <span className="ms_nav--bean"></span>
+                <p>MONK & SMUGGLER</p>
+              </a>
+            </li>
+            <li className="ms_nav--menu-item">
+              <a href="#os_openinghours" onClick={() => setShowNav(false)}>
+                <span className="ms_nav--bean"></span>
+                <p>OPENING HOURS</p>
+              </a>
+            </li>
+            <li className="ms_nav--menu-item">
+              <a href="#os_story" onClick={() => setShowNav(false)}>
+                <span className="ms_nav--bean"></span>
+                <p>OUR STORY</p>
+              </a>
+            </li>
+            <li className="ms_nav--menu-item">
+              <a href="#os_contact">
+                <span className="ms_nav--bean"></span>
+                <p>GET IN TOUCH</p>
+              </a>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <button
+          onClick={() => setShowNav(true)}
+          className="ms_nav--Btn ms_Btn flex col gap-s closedNav">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      )}
+    </nav>
+  );
+};
