7515179b90a253b8c1e49afbe9de2304071f0236
Nath Duncan <nathan.duncan@outserveweb.co.uk>
2024-06-26 15:40:47 +0100

    build out landing page

--- src/components/sections/landingPage.tsx
+++ src/components/sections/landingPage.tsx
@@ -1,3 +1,28 @@
-export const LandingPage = () => {
-  <p>Landing Page</p>;
-};
+import logo from "../../images/MonkandSmugglerLogoOnBlack.svg";
+
+const comments = [
+  "Feel like i'm on holiday",
+  "Finally, good vegan options",
+  "You've brought Shoreditch to Stafford",
+  "VEGAN CAKES!",
+  "Best coffee shop in town!",
+  "Best cappuccino I've ever had",
+  "I love the clearly labelled vegan",
+  "You're giving me house decór inspo!",
+];
+
+export const LandingPage = () => (
+  <div className="pageContainer">
+    <div className="logoOverlay"></div>
+    <div className="logoContainer">
+      <img src={logo} className="site-logo" alt="Monk & Smuggler logo" />
+    </div>
+    <div id="os_main" className="metaContainer">
+      <h1>Monk & Smuggler</h1>
+      <h2>Coffee | Books | Culture</h2>
+    </div>
+    <div className="textContainer">
+      <h3>Something is brewing...</h3>
+    </div>
+  </div>
+);
