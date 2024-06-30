56b0db17e4f1785281158d518638da4a684b9819
Nath Duncan <nathan.duncan@outserveweb.co.uk>
2024-06-30 19:26:19 +0100

    refactor logo

--- src/components/tools/logo.tsx
+++ src/components/tools/logo.tsx
@@ -1,13 +1,11 @@
 import { svgLogo } from "../../images/MonkandSmugglerLogoOnBlack";
 import img from "../../images/MonkandSmugglerLogoOnBlack.png";
 
-export const Logo = ({ type, alt }: { type?: string; alt?: string }) => {
-  console.log("image", img);
-  return type && type === "png" ? (
+export const Logo = ({ type, alt }: { type?: string; alt?: string }) =>
+  type && type === "png" ? (
     <img src={img} alt="Monk & Smuggler logo" style={{ maxWidth: "100%" }} />
   ) : type === "svg" ? (
     svgLogo
   ) : (
     <></>
   );
-};
