417a2eb138d8cd0f5937bf7ac27acb920844ea71
Nath Duncan <nathan.duncan@outserveweb.co.uk>
2024-06-30 18:12:55 +0100

    refactor logo file

--- src/components/tools/logo.tsx
+++ src/components/tools/logo.tsx
@@ -1,20 +1,13 @@
-import { useEffect, useRef } from "react";
 import { svgLogo } from "../../images/MonkandSmugglerLogoOnBlack";
+import img from "../../images/MonkandSmugglerLogoOnBlack.png";
 
 export const Logo = ({ type, alt }: { type?: string; alt?: string }) => {
-  const image = svgLogo;
-
-  const useApp = (image: string) => {
-    const svgWrapperRef = useRef<HTMLDivElement>(null);
-    useEffect(() => {
-      // @ts-ignore
-      svgWrapperRef.current.innerHTML = image;
-    }, [image]);
-    return {
-      svgWrapperRef,
-    };
-  };
-
-  //   const { svgWrapperRef } = useApp(image);
-  return svgLogo;
+  console.log("image", img);
+  return type && type === "png" ? (
+    <img src={img} alt="Monk & Smuggler logo" style={{ maxWidth: "100%" }} />
+  ) : type === "svg" ? (
+    svgLogo
+  ) : (
+    <></>
+  );
 };
