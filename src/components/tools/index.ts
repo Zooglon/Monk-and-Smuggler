74778b98728a32fc15af355d48325185fd4f97dd
Nath Duncan <nathan.duncan@outserveweb.co.uk>
2024-07-01 20:02:18 +0100

    update index with new file

--- src/components/tools/index.ts
+++ src/components/tools/index.ts
@@ -4,5 +4,6 @@ import { Paragraph } from "./paragraph";
 import { Subtitle } from "./subTitle";
 import { Title } from "./title";
 import { Logo } from "./logo";
+import { WindowImg } from "./windowImg";
 
-export { Box, Carousel, Paragraph, Subtitle, Title, Logo };
+export { Box, Carousel, Paragraph, Subtitle, Title, Logo, WindowImg };
